c2fe0d3f25c88fa2984ed6ee5d285bf2b11b426b
rabbi4570 <fozlerabbi1120@gmail.com>
2025-07-19 00:13:32 +0600

    load data and displaynote

--- script.js
+++ script.js
@@ -5,12 +5,25 @@ const noteContainer = document.getElementById('notesContainer');
 // initialize an empty array to store notes
 const notes = [];
 
+
+window.addEventListener('DOMContentLoaded', loadData())
+
+
+// loadData from local Storage
+function loadData () {
+    const savedData = JSON.parse(localStorage.getItem('notes')) || '[]';
+    notes.push(...savedData);
+    notes.forEach(note => displayNote(note))
+    console.log(savedData)
+}
+
 // handle click event on 'addNote' button
 addBtn.addEventListener('click', () =>{
     const note = noteInput.value.trim();
     if(!note) return;
     notes.push(note)
     displayNote(note)
+    savedToLocalStorage()
     noteInput.value = '';
 
 })
@@ -21,5 +34,25 @@ function displayNote(note){
     const deleteNote = document.createElement('button');
     deleteNote.innerText = 'Delete';
     deleteNote.style.marginLeft = '10px';
+    li.appendChild(deleteNote)
     noteContainer.appendChild(li)
+
+    // delte notes
+    deleteNote.addEventListener('click', () =>{
+        li.remove()
+        removeNotesLocalStorage(note)
+    })
+}
+
+
+function savedToLocalStorage(){
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+function removeNotesLocalStorage(note){
+    const index = notes.indexOf(note)
+    if(index > -1){
+        notes.splice(index, 1);
+        savedToLocalStorage()
+    }
 }
