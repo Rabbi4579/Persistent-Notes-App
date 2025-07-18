d52e604ac390b8847a6f0a0091b8e2e5b0d61397
rabbi4570 <fozlerabbi1120@gmail.com>
2025-07-18 23:47:56 +0600

    feat: add functionality to create and display notes

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const noteInput = document.getElementById('noteInput');
 const addBtn = document.getElementById('addNote');
-const nodeContainer = document.getElementById('notesContainer');
+const noteContainer = document.getElementById('notesContainer');
 
+// initialize an empty array to store notes
+const notes = [];
 
+// handle click event on 'addNote' button
+addBtn.addEventListener('click', () =>{
+    const note = noteInput.value.trim();
+    if(!note) return;
+    notes.push(note)
+    displayNote(note)
+    noteInput.value = '';
+
+})
+
+function displayNote(note){
+    const li = document.createElement('li');
+    li.textContent = note;
+    const deleteNote = document.createElement('button');
+    deleteNote.innerText = 'Delete';
+    deleteNote.style.marginLeft = '10px';
+    noteContainer.appendChild(li)
+}
